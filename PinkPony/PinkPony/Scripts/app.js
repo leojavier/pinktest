ac76fe41dbeb38cece1cdc8b554b1e5e91b7b7e0
Leo Javier <leo@impactrepublic.com>
2015-06-09 11:37:40 -0400

    first checking

---
+++ PinkPony/PinkPony/Scripts/app.js
@@ -0,0 +1,45 @@
+﻿
+var carousel;
+$(document).ready(function () {
+
+    $("#owl-demo").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        // Most important owl features
+        items: 3,
+        itemsCustom: false,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 2],
+        itemsTablet: [768, 2],
+        itemsTabletSmall: false,
+        itemsMobile: [580, 1],
+        singleItem: false,
+        itemsScaleUp: false,
+
+
+        //Autoplay
+        autoPlay: false,
+        stopOnHover: false,
+
+        // Navigation
+        navigation: true,
+        navigationText: ["", ""],
+        rewindNav: true,
+        scrollPerPage: false,
+
+        //Pagination
+        pagination: false,
+        paginationNumbers: false,
+
+        // Responsive
+        responsive: true,
+        responsiveRefreshRate: 200,
+        responsiveBaseWidth: window,
+
+        //Auto height
+        autoHeight: false,
+
+    });
+    carousel = $("#owl-demo").data('owlCarousel');
+});
